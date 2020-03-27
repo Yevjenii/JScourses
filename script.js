d35a5ba89955af4c8dae2ba871d4a1290574dcab
Evgeniy Petukhov <moddii16@gmail.com>
2020-03-28 00:30:20 +0300

    убрал лишнее в скриптах

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ let money = 1000, income, addExpenses, deposit, mission, period;
 alert('how much money?');
 console.log('any text! money:' + money);
 
-alert('work with git');
 // пробовал горячие клавиши
